539d3bf6e90186c5d21561a85ce279a04b99b9ca
ljz521 <ljz521@qq.com>
2022-01-21 13:02:15 +0800

    线程安全的饿汉式

--- src/main/java/com/xidian/single_ton/single01/SingleTest.java
+++ src/main/java/com/xidian/single_ton/single01/SingleTest.java
@@ -4,6 +4,10 @@ package com.xidian.single_ton.single01;
  * @author ljz990305
  * @create 2022-01-20 18:17
  */
+
+/**
+ * 饿汉式，线程安全，可能会有空间浪费(不确定类的装在次数，伴随着类的加载而加载，通过类加载机制保证线程安全)
+ */
 public class SingleTest {
     public static void main(String[] args) {
         SingleTon01 instance1 = SingleTon01.getInstance();
